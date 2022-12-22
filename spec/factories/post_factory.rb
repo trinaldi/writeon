02d99935d1256ae8a88c887047727dfb973e5123
Tiago Rinaldi <trinaldi@gmail.com>
2022-12-22 17:35:33 -0300

    Update post factory

--- spec/factories/post_factory.rb
+++ spec/factories/post_factory.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :post do
     title { Faker::Book.title }
     body { Faker::Quote.famous_last_words }
-    comment { build(:comment) }
+    comment { [build(:comment)] }
   end
 end
