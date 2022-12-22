af820bac76b1b7700e47735fd0cddcffcd5b10be
Tiago Rinaldi <trinaldi@gmail.com>
2022-12-22 17:36:18 -0300

    Add GraphQL query for posts

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -3,22 +3,10 @@ module Types
     include GraphQL::Types::Relay::HasNodeField
     include GraphQL::Types::Relay::HasNodesField
 
-    field :comments, [Types::CommentType], null: false
-    field :posts, [Types::PostType], null: false
-    field :post, Types::PostType, null: false do
-      argument :id, ID, required: true
-    end
-
-    def comments
-      Comment.all
-    end
+    field :posts, [Types::PostType], null: false, description: 'All posts'
 
     def posts
       Post.all
-    end
-
-    def post(id:)
-      Post.find(id)
     end
   end
 end
---
+++ spec/app/graphql/queries/posts_query_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+
+describe 'Posts Query', type: :request do
+  include_context 'GraphQL Client'
+
+  context 'when calling posts query' do
+    let!(:posts) { create(:post) }
+    let(:query) do
+      <<-'GRAPHQL'
+    {
+      posts {
+        id
+        title
+        body
+        comment {
+          id
+          name
+          message
+        }
+      }
+    }
+      GRAPHQL
+    end
+    let(:posts_query_response) { graph_response[:data] }
+    let(:search_posts) do
+      {
+        'posts' => [{
+          'id' => posts.id.to_s,
+          'title' => posts.title,
+          'body' => posts.body,
+          'comment' => posts.comment.map do |c|
+            {
+              'id' => c.id.to_s,
+              'name' => c.name,
+              'message' => c.message
+            }
+          end
+        }]
+      }
+    end
+
+    before do
+      post_graph(query)
+    end
+
+    it 'returns the post' do
+      expect(posts_query_response).to eq(search_posts)
+    end
+  end
+end
