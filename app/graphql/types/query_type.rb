85894ead8f076c5432e73c1b53bf14a78c4f5528
Tiago Rinaldi <trinaldi@gmail.com>
2023-01-05 13:16:13 -0300

    Add GraphQL query for latest 30 days posts

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -3,10 +3,16 @@ module Types
     include GraphQL::Types::Relay::HasNodeField
     include GraphQL::Types::Relay::HasNodesField
 
+    field :month_prior_posts, [Types::PostType], null: false,
+                                                 description: 'Posts from a month ago'
     field :posts, [Types::PostType], null: false, description: 'All posts'
 
     def posts
       Post.all
+    end
+
+    def month_prior_posts
+      Post.where(created_at: { '$gte': 1.month.ago }).order(created_at: 1)
     end
   end
 end
