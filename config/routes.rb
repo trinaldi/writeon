f0dc849a74ce9629d940cd587ad998a3dc15e554
Tiago Rinaldi <trinaldi@gmail.com>
2026-03-26 09:54:50 -0300

    revamp: cleanup after me

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,4 @@ require 'graphiql/rails'
 Rails.application.routes.draw do
   mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql' if Rails.env.development?
   post '/graphql', to: 'graphql#execute'
-  resources :posts
-  resources :comments
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
