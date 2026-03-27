9e18290a55ae18e7d43e3fadeddbc4ea416ec3d7
Tiago Rinaldi <trinaldi@gmail.com>
2026-03-27 15:01:20 -0300

    fiz add day mutation

--- app/graphql/mutations/add_day.rb
+++ app/graphql/mutations/add_day.rb
@@ -5,10 +5,19 @@ module Mutations
     field :day, Types::DayType, null: true
 
     def resolve(date:)
-      {
-        day: nil,
-        errors: ["cheguei aqui"]
-      }
+      begin
+        day = Day.create(date: date)
+
+        {
+          day: day.persisted? ? day : nil,
+          errors: day.errors.full_messages
+        }
+      rescue => e
+        {
+          day: nil,
+          errors: [e.message]
+        }
+      end
     end
   end
 end
